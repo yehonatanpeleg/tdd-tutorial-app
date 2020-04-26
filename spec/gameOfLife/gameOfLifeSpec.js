5163f597047878939d90c13316b0575f0db9823a
yehonatanpelege <yehonatanp@wix.com>
2020-04-26 15:15:21 +0300

    first commit

---
+++ spec/gameOfLife/gameOfLifeSpec.js
@@ -0,0 +1,147 @@
+describe("Generate Game Of Life Grid", function name() {
+  const generateGrid = require("../../src/gameOfLife/gameOfLife").generateGrid;
+
+  it("should return a grid with 5 on 5", function() {
+    expect(generateGrid(5, 5).length).toEqual(5);
+    expect(generateGrid(5, 5)[0].length).toEqual(5);
+  });
+
+  it("should return a grid with 10 on 8", function() {
+    expect(generateGrid(10, 8).length).toEqual(10);
+    expect(generateGrid(10, 8)[0].length).toEqual(8);
+  });
+
+  it("should return a grid with cell elements", function() {
+    expect(generateGrid(10, 8)[5][2].alive).toBeDefined();
+    expect(generateGrid(10, 8)[5][2].location.row).toBeDefined();
+    expect(generateGrid(10, 8)[5][2].location.col).toBeDefined();
+  });
+
+  it("should return a grid with cell elements with correct values", function() {
+    expect(generateGrid(10, 8)[5][2].alive).toBeFalsy();
+    expect(generateGrid(10, 8)[5][2].location.row).toEqual(5);
+    expect(generateGrid(10, 8)[5][2].location.col).toEqual(2);
+    expect(generateGrid(10, 8)[5][6].alive).toBeFalsy();
+    expect(generateGrid(10, 8)[5][6].location.row).toEqual(5);
+    expect(generateGrid(10, 8)[5][6].location.col).toEqual(6);
+  });
+});
+
+describe("Retrieve Cell And Neighbors", function name() {
+  const generateGrid = require("../../src/gameOfLife/gameOfLife").generateGrid;
+  const retrieveCellNeighbors = require("../../src/gameOfLife/gameOfLife")
+    .retrieveCellNeighbors;
+  var grid;
+
+  beforeEach(function name() {
+    grid = generateGrid(5, 7);
+  });
+
+  it("should return correct number of  neighbors", function() {
+    expect(retrieveCellNeighbors(grid, 0, 0).length).toEqual(3);
+    expect(retrieveCellNeighbors(grid, 4, 5).length).toEqual(5);
+    expect(retrieveCellNeighbors(grid, 2, 2).length).toEqual(8);
+  });
+
+  it("should return correct neighbors", function() {
+    expect(retrieveCellNeighbors(grid, 0, 0)).toContain({
+      alive: false,
+      location: {
+        row: 0,
+        col: 1,
+      },
+    });
+    expect(retrieveCellNeighbors(grid, 0, 0)).toContain({
+      alive: false,
+      location: {
+        row: 1,
+        col: 0,
+      },
+    });
+    expect(retrieveCellNeighbors(grid, 0, 0)).toContain({
+      alive: false,
+      location: {
+        row: 1,
+        col: 1,
+      },
+    });
+    expect(retrieveCellNeighbors(grid, 4, 6)).toContain({
+      alive: false,
+      location: {
+        row: 3,
+        col: 6,
+      },
+    });
+
+    expect(
+      retrieveCellNeighbors(grid, 5, 6).includes({
+        alive: false,
+        location: {
+          row: 5,
+          col: 6,
+        },
+      })
+    ).toBeFalsy();
+  });
+});
+
+describe("Retrieve next generation", function name() {
+  const generateGrid = require("../../src/gameOfLife/gameOfLife").generateGrid;
+  const retrieveCellNeighbors = require("../../src/gameOfLife/gameOfLife")
+    .retrieveCellNeighbors;
+  const retrieveNextGeneration = require("../../src/gameOfLife/gameOfLife")
+    .retrieveNextGeneration;
+  var grid;
+
+  beforeEach(function name() {
+    grid = generateGrid(5, 7);
+  });
+
+  it("should return grid with same length", function() {
+    expect(retrieveNextGeneration(grid).length).toEqual(5);
+    expect(retrieveNextGeneration(grid)[0].length).toEqual(7);
+  });
+
+  it("cell [2][3] need to die of loneliness", function() {
+    grid[2][3].alive = true;
+    expect(retrieveNextGeneration(grid)[2][3].alive).toBeFalsy();
+  });
+
+  it("cell [2][3] need to die of crowdedness", function() {
+    grid[2][3].alive = true;
+    grid[1][3].alive = true;
+    grid[1][2].alive = true;
+    grid[3][3].alive = true;
+    expect(retrieveNextGeneration(grid)[2][3].alive).toBeFalsy();
+  });
+
+  it("cell [2][3] need to reborn", function() {
+    grid[2][3].alive = false;
+    grid[1][3].alive = true;
+    grid[1][2].alive = true;
+    grid[3][3].alive = true;
+    expect(retrieveNextGeneration(grid)[2][3].alive).toBeTruthy();
+  });
+
+  it("cell [4][4] need to die of loneliness", function() {
+    grid[4][4].alive = true;
+    grid[4][3].alive = true;
+    expect(retrieveNextGeneration(grid)[2][3].alive).toBeFalsy();
+  });
+
+  it("cell [4][4] need to die of crowdedness", function() {
+    grid[4][4].alive = true;
+    grid[3][5].alive = true;
+    grid[4][5].alive = true;
+    grid[4][3].alive = true;
+    expect(retrieveNextGeneration(grid)[4][4].alive).toBeFalsy();
+  });
+
+  it("cell [4][4] need to reborn", function() {
+    grid[4][4].alive = false;
+    grid[3][5].alive = true;
+    grid[4][5].alive = true;
+    grid[4][3].alive = true;
+    expect(retrieveNextGeneration(grid)[4][4].alive).toBeTruthy();
+  });
+});
---
+++ src/gameOfLife/gameOfLife.js
@@ -0,0 +1,89 @@
+var lodash = require("lodash");
+
+function generateGrid(rows, cols) {
+  // Creating grid
+  var grid = new Array(rows).fill(0);
+  grid = grid.map((element) => {
+    return new Array(cols).fill(0);
+  });
+
+  // Initializing grid
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      grid[i][j] = {
+        alive: false,
+        location: {
+          row: i,
+          col: j,
+        },
+      };
+    }
+  }
+
+  return grid;
+}
+
+function retrieveCellNeighbors(grid, cellRow, cellCol) {
+  // Initializing neighbors
+  var neighbors = [];
+
+  for (var i = cellRow - 1; i <= cellRow + 1; i++) {
+    for (var j = cellCol - 1; j <= cellCol + 1; j++) {
+      if (
+        !(i === cellRow && j == cellCol) &&
+        0 <= i &&
+        i < grid.length &&
+        0 <= j &&
+        j < grid[0].length
+      ) {
+        neighbors.push(grid[i][j]);
+      }
+    }
+  }
+
+  return neighbors;
+}
+
+function countAliveNeighbors(neighbors) {
+  // Initializing
+  var aliveCount = 0;
+
+  // Counting number of alive neighbors
+  for (neighbor of neighbors) {
+    if (neighbor.alive) {
+      aliveCount++;
+    }
+  }
+
+  return aliveCount;
+}
+
+function retrieveNextGeneration(grid) {
+  // Generating new grid from old grid
+  var newGrid = lodash.cloneDeep(grid);
+
+  // Calculating next generation
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[0].length; j++) {
+      // Retrieving count of alive neighbors
+      var neighborsAlive = countAliveNeighbors(
+        retrieveCellNeighbors(grid, i, j)
+      );
+
+      // If current cell alive, it can die of loneliness or crowdedness
+      if (grid[i][j].alive && (neighborsAlive <= 1 || neighborsAlive >= 3)) {
+        newGrid[i][j].alive = false;
+      }
+      // If current cell dead, it can be reborn
+      else if (!grid[i][j].alive && neighborsAlive === 3) {
+        newGrid[i][j].alive = true;
+      }
+    }
+  }
+
+  return newGrid;
+}
+
+module.exports.generateGrid = generateGrid;
+module.exports.retrieveCellNeighbors = retrieveCellNeighbors;
+module.exports.retrieveNextGeneration = retrieveNextGeneration;
